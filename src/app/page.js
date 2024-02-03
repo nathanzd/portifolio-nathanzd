3918dbf7a813a99b8ce904df3f7cd387778e50b9
nathanzd <nathan.fe.dias@gmail.com>
2024-02-03 19:43:03 -0300

    googletagmanager

--- src/app/page.js
+++ src/app/page.js
@@ -37,7 +37,7 @@ export default function Home() {
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-2GCC51TB88"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
+      function gtag(){dataLayer.push(arguments)}
       gtag('js', new Date());
 
       gtag('config', 'G-2GCC51TB88');
