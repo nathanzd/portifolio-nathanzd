5a0e7682cce869d7d2282b7f0c815a8c402f18e3
nathanzd <nathan.fe.dias@gmail.com>
2024-02-03 19:40:29 -0300

    googletagmanager

--- src/app/page.js
+++ src/app/page.js
@@ -34,6 +34,17 @@ export default function Home() {
     },[])
   return (
    <body>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2GCC51TB88"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-2GCC51TB88');
+    </script>
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link href="https://fonts.googleapis.com/css2?family=Gabarito:wght@400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"/>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <HeaderComponent black={blackHeader}/>
     
